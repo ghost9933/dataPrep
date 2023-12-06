bc2e9ae9cfa1b550f3a46350f939637a0544156d
ghost9933 <mhatrenikhil36@gmail.com>
2023-12-06 10:44:19 -0600

    dependency update by mana

--- node-dataprep/main.js
+++ node-dataprep/main.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log("User ID:", userID.value); // Log the userID value
 
             try {
-                const response = await axios.post('localhost:5000/upload', formData, {
+                const response = await axios.post('34.174.88.226:5000/upload', formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
