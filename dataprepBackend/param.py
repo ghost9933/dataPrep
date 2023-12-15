5ba4b55c7209d307b79ee54bcc345ae276689e81
ghost9933 <mhatrenikhil36@gmail.com>
2023-12-15 00:05:55 -0600

    revert

--- dataprepBackend/param.py
+++ dataprepBackend/param.py
@@ -1,11 +1,11 @@
 PROJECT_ID = "planar-depth-403402"
-JOB_NAME = 'dataprep-job-ash-0087'
+JOB_NAME = 'dataprep-0087'
 TEMP_DIR = 'gs://inclasslab3/temp'
 STAGING_LOCATION = 'gs://inclasslab3/staging'
 REGION = 'us-south1'
 SERVICE_ACCOUNT_EMAIL = "project5333@planar-depth-403402.iam.gserviceaccount.com"
 import os
-KEY_FILE = os.path.join(os.path.dirname(__file__),'/home/mhatrenikhil36/keys.json')
+KEY_FILE = os.path.join(os.path.dirname(__file__),'keys.json')
 input_files = 'gs://dataprep-bucket-001/Raw-Data/subset_dataset.csv'
 JOB_FILE_PATH='gs://inclasslab3/dataProcJob.py'
 CLUSTER_NAME = 'test'
